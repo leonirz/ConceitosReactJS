c8d97431864c1751722b9ea602221ce7271739d8
Leonir Zimmermann <leonirz@gmail.com>
2020-07-25 18:21:12 -0300

    CR test ReactJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import api from './services/api.js';  // com js ou não?
 
 import "./styles.css";
 
 function App() {
+  
+  //array de repositórios - local FRONTEND
+  const [repositories, setRepositories] = useState([]); 
+
+  useEffect(() => {
+    api.get('repositories').then(response=>{
+        //console.log(response);
+        setRepositories(response.data);
+    }); //rota sem contar a base-URL
+  }, []); // uma função disparada uma vez quando . O array [] é conhecido como array de dependências
+
+  
   async function handleAddRepository() {
     // TODO
+
+    const response = await api.post('repositories', {
+      //title: 'Desafio ReactJS',        // title: `Desafio ReactJS ${Date.now()}`,
+      id: 0,
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"],
+  });
+
+  const newRepository = response.data;
+
+  setRepositories([...repositories, newRepository]);
+  
+  
+  //console.log("olha ali");
+
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+
+    await api.delete(`/repositories/${id}`);
+    const newRepositories = repositories.filter(repository => repository.id !== id);
+    setRepositories(newRepositories);
+
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(theRepository => <li key={theRepository.id}>{theRepository.title}<button onClick={() => handleRemoveRepository(theRepository.id)}>Remover</button></li>
+                                           
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
